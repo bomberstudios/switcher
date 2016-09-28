f027b2468602b6af2fb4ecfda79a093a3d8f5dfa
Ale Muñoz <bomberstudios@gmail.com>
2016-09-28 08:51:42 +0200

    Code cleanup

--- js/background.js
+++ js/background.js
@@ -1,26 +1,17 @@
 var sites = {};
 var groups = [];
 loadSettings();
+
 function loadSettings() {
-    sites = JSON.parse(localStorage.sites) || {};
-    groups = JSON.parse(localStorage.groups) || [];
+  // TODO: fix JS error when first running the extension and settings are empty
+  sites = JSON.parse(localStorage.sites) || {};
+  groups = JSON.parse(localStorage.groups) || [];
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-      console.log(request);
-      // console.log(sites);
-      // console.log(groups);
-      // console.log(sites[request.site]);
-      // console.log(groups[sites[request.site]]);
-      if(sites[request.site] !== undefined) {
-          //console.log("gevonden");
-          sendResponse(groups[sites[request.site]]);
-      }
-
-      if(sites[request.site] !== undefined && request.popup) {
-          sendResponse(groups[sites[request.site]]);
-      }
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(sites[request.site] !== undefined && request.popup) {
+    sendResponse(groups[sites[request.site]]);
+  }
 });
 
 // Listen for any changes to the URL of any tab.
@@ -29,63 +20,33 @@ chrome.tabs.onCreated.addListener(createListener);
 chrome.tabs.onReplaced.addListener(replaceListener);
     
 function replaceListener(added, removed) {
-    chrome.tabs.get(added, createListener);
+  chrome.tabs.get(added, createListener);
 }
                                    
 function createListener(tab) {
-    showSwitcher(tab);
+  showSwitcher(tab);
 }
 
 function updateListener(tabId, changeInfo, tab) {
-    if(tabId && changeInfo && changeInfo.status && changeInfo.status == "loading") {
-        showSwitcher(tab);
-    }
+  if(tabId && changeInfo && changeInfo.status && changeInfo.status == "loading") {
+    showSwitcher(tab);
+  }
 }
 
 function showSwitcher(tab) {
-    if(tab && tab.id && tab.url) {
-        var uri = URI(tab.url);
-        if(uri) {
-            var origin = uri.protocol() + "://" + uri.hostname();
-            if (uri.port()) {
-              origin += ":" + uri.port();
-            }
-            // console.log(origin);
-            // console.log(tabId);
-            // console.log(sites[origin]);
-            if(sites[origin] !== undefined) {
-                //console.log("showing");
-                chrome.pageAction.show(tab.id);
-            }
-            else {
-                //console.log("hiding");
-                chrome.pageAction.hide(tab.id);
-            }
-        }
+  if(tab && tab.id && tab.url) {
+    var uri = URI(tab.url);
+    if(uri) {
+      var origin = uri.protocol() + "://" + uri.hostname();
+      if (uri.port()) {
+        origin += ":" + uri.port();
+      }
+      if(sites[origin] !== undefined) {
+        chrome.pageAction.show(tab.id);
+      }
+      else {
+        chrome.pageAction.hide(tab.id);
+      }
     }
-
+  }
 }
-
-// chrome.extension.onRequest.addListener(function(request, sender, callback) {
-//     if(request == 'getGroup') {
-//         var group = {};
-//         if(sites[request.origin] != undefined) {
-//             group = groups[sites[request.origin]];
-//             callback(group);
-//             return;
-//         }
-//     }
-
-// });
-
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if(request.action == "getGroup") {
-//       var group = {};
-//       if (sites[request.origin] != undefined) {
-//         group = groups[sites[request.origin]];
-//       }
-//       sendResponse({group: group, keypressed: request.keypressed});
-//     }
-//   }
-// );
--- js/popup.js
+++ js/popup.js
@@ -1,50 +1,50 @@
 var tabId = 0;
+
 window.onload = function() {
-    var bg = chrome.extension.getBackgroundPage();
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        if(tabs && tabs.length > 0) {
-            var uri = URI(tabs[0].url);
-            tabId = tabs[0].id;
-            if(uri) {
-                var origin = uri.protocol() + "://" + uri.hostname();
-                if (uri.port()) {
-                  origin += ":" + uri.port();
-                }
-                chrome.runtime.sendMessage({site:origin, popup:1}, function(response) {
-                    var keys = Object.keys(response).sort();
-                    var html = "<ul>";
-                    keys.forEach(function(key) {
-                        if (key != 'carry_params') {
-                            if(response[key] == origin) {
-                                html += "<li><span>" + response[key] + uri.pathname() + "</span></li>";
-                            }
-                            else {
-                                var href = response[key];
-                                if(response.carry_params) {
-                                    href = href + uri.pathname();
-                                    // href = href + uri.search();
-                                }
-                                html += "<li><a href='" + href + "'>" + href + "</a></li>";
-                            }
-                        }
-                    });
-                    html += "</ul>";
-                    var links = document.getElementById("links");
-                    links.innerHTML = html;
-                });
-            }
+  var bg = chrome.extension.getBackgroundPage();
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if(tabs && tabs.length > 0) {
+      var uri = URI(tabs[0].url);
+      tabId = tabs[0].id;
+      if(uri) {
+        var origin = uri.protocol() + "://" + uri.hostname();
+        if (uri.port()) {
+          origin += ":" + uri.port();
         }
-    });    
+        chrome.runtime.sendMessage({site:origin, popup:1}, function(response) {
+          var keys = Object.keys(response).sort();
+          var html = "<ul>";
+          keys.forEach(function(key) {
+            if (key != 'carry_params') {
+              if(response[key] == origin) {
+                html += "<li><span>" + response[key] + uri.pathname() + "</span></li>";
+              }
+              else {
+                var href = response[key];
+                if(response.carry_params) {
+                  href = href + uri.pathname();
+                }
+                html += "<li><a href='" + href + "'>" + href + "</a></li>";
+              }
+            }
+          });
+          html += "</ul>";
+          var links = document.getElementById("links");
+          links.innerHTML = html;
+        });
+      }
+    }
+  });    
 };
 
 
 window.onclick = function(event) {
-    if(event.target.nodeName == 'A') {
-        if(event.target.href) {
-            if(tabId) {
-                chrome.tabs.update(tabId, {"url": event.target.href});
-                window.close();
-            }
-        }
+  if(event.target.nodeName == 'A') {
+    if(event.target.href) {
+      if(tabId) {
+        chrome.tabs.update(tabId, {"url": event.target.href});
+        window.close();
+      }
     }
+  }
 };
